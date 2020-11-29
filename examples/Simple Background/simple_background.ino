1db36d1cef290a3b4aec3abcfe2058c944e74865
ellima <me@4ellingers.de>
2020-11-29 16:48:35 +0100

    Adapted example to sun direction option

--- examples/Simple Background/simple_background.ino
+++ examples/Simple Background/simple_background.ino
@@ -3,12 +3,13 @@
 #define LED_PIN 0 // sets the output pin for the data to the strip. See the pinout for your microcontroller
 #define LED_COUNT 8 // sets the total number of pixels in your setup
 #define MINUTES 5 // sets the real-time in minutes from sunrise to sunset
+#define OPPOSITE_START false // switch to 'true' on order to reverse the "sun's" direction
 
 Background * background = nullptr;
 
 void setup() {
     // initialize Background object
-    background = new Background(LED_PIN, LED_COUNT, MINUTES); // Parameters are the quantities defined above
+    background = new Background(LED_PIN, OPPOSITE_START, LED_COUNT, MINUTES); // the parameters are the quantities defined above
 }
 
 void loop() {
